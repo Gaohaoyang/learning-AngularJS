72ffd19cc5a8d01243d4e887656af47cb9868c03
chuan shi(haoyang gao) <ghy118432@alibaba-inc.com>
2016-07-09 20:50:31 +0800

    format and start html dom

--- still-learn/select/index.js
+++
@@ -1,7 +0,0 @@
-var selectApp  = angular.module('selectApp',[])
-
-selectApp.controller('myCtrl',function ($scope,$http) {
-    $http.get('https://api.github.com/users/Gaohaoyang/followers').success(function (response) {
-        $scope.followers = response
-    })
-})
